a342517248f410a603932800ff7cd2febb3b7b11
yunsusu <rladbstn1212@gmail.com>
2024-01-26 16:59:12 +0900

    feat : axios기본 세팅 / 주스탄드 설치

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,5 +1,37 @@
-const Test = () => {
-  return <p>test</p>;
-};
+import Head from "next/head";
+import axios from "@/lib/axios";
 
-export default Test;
+export default function Test() {
+  async function getFolders() {
+    const res = await axios.post(`auth/login`, {
+      email: "codeit@codeit.com",
+      password: "123456789",
+    });
+    console.log(res.data.accessToken);
+    localStorage.removeItem("login");
+    localStorage.setItem("login", res.data.accessToken);
+  }
+
+  async function getFd() {
+    const res = await axios.get(`users/me`);
+    console.log(res);
+  }
+
+  const click = () => {
+    getFolders();
+  };
+  const clickclick = () => {
+    getFd();
+  };
+
+  return (
+    <>
+      <button style={{ width: "200px", height: "200px" }} onClick={click}>
+        로그인
+      </button>
+      <button style={{ width: "200px", height: "200px" }} onClick={clickclick}>
+        내 정보 보기
+      </button>
+    </>
+  );
+}
