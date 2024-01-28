f65292f8d648b32cdbc79f2b04353dd63e1f726e
yunsusu <rladbstn1212@gmail.com>
2024-01-28 14:54:32 +0900

    fix : input 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,10 @@
 import Head from "next/head";
-import Input from "@/components/input";
 
 export default function Home() {
   return (
     <>
-      <Input
-        title="비밀번호 확인"
-        palceholder="비밀번호를 한번 더 입력해 주세요"
-        data="pwd"
-      />
-      <Input
-        title="비밀번호"
-        palceholder="비밀번호를 한번 더 입력해 주세요"
-        data="pwd"
-      />
-      <Input
-        title="이메일"
-        palceholder="이메일를 한번 더 입력해 주세요"
-        data="이메일"
-      />
-      <Input
-        title="닉네임"
-        palceholder="닉네임를 한번 더 입력해 주세요"
-        data="닉네임"
-      />
+      <div>Hi HomePage </div>
+      <div> hii</div>
     </>
   );
 }
