fa4c4281e31211974c15f121bba5f42f28e50ef5
bumsly <65000254+bumsly@users.noreply.github.com>
2024-01-28 23:48:41 +0900

    app 컴포넌트에서 test용 ToDoModal 제거하기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,11 @@
 import GlobalStyle from "@/styles/global-style";
 import type { AppProps } from "next/app";
 
-import ToDoModal from "@/components/toDoModal/ToDoModal";
-import mock from "../components/toDoModal/mock";
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
       <Component {...pageProps} />
-      <ToDoModal {...mock} />
     </>
   );
 }
